771e1769f2a9f7dd3882fba57952e09f1ee9fead
Carelia Ibarra <carelia2001@hotmail.com>
2021-05-13 11:53:18 -0700

    [DOC] Added README and Act1
    Repository description and first activity.

---
+++ Act1_A01251463.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <ctime>
+#include <windows.h>
+
+using namespace std;
+
+class Vendedor {                    // clase vendedor
+    public:
+        string Cve_Vendedor;        // clave del vendedor
+        string Nombre;              // nombre del vendedor
+};
+
+class Inventario {
+    public:
+        string Cve_Articulo;
+        string Descripcion;
+        double Precio;
+};
+
+class Factura {
+    public:
+        string NoFactura;
+        string Cve_Vendedor;
+        string Cve_Articulo;
+        int Cantidad;
+};
+
+string GETDATE(){
+    char out[14];
+    std::time_t t=std::time(NULL);
+    std::strftime(out, sizeof(out), "%Y%m%d%H%S", std::localtime(&t));
+    Sleep(2000);  // pauses for 10 seconds
+    return out;
+};
+
+void GeneraFactura(int contadorFactura, Factura arregloF[], Vendedor v1, Inventario i2, int Cant){
+
+    Factura f1;
+    f1.Cve_Vendedor = v1.Cve_Vendedor;
+    f1.Cve_Articulo = i2.Cve_Articulo;
+    f1.Cantidad = Cant;
+    f1.NoFactura = "F" + GETDATE();
+    arregloF[contadorFactura] = f1;
+};
+
+int main() {
+
+    Vendedor arregloV[2];
+    Inventario arregloI[4];
+    Factura arregloF[5];
+
+    Vendedor v1 = {"v100","Don Julio Estrella el Grande"}; // variable v1, vendedor 1
+    Vendedor v2 = {"v200","Dona Esperanza Luna"};
+    arregloV[0] = v1;           // metiendo los vendedores al arreglo
+    arregloV[1] = v2;
+
+    Inventario i1 = {"i500","Tornillo sin fin",10.0};
+    Inventario i2 = {"i100","Pinon Cremallera",10.0};
+    Inventario i3 = {"i700","Angulo de Ackerman",10.0};
+    arregloI[0] = i1;
+    arregloI[1] = i2;
+    arregloI[2] = i3;
+
+    int contadorFactura = 0;
+    
+    GeneraFactura(contadorFactura, arregloF, v1, i2, 32);
+
+    // imprimimos todas las facturas
+    cout << "Facturas Generadas "<< endl;
+    cout << "No" <<"\t\t"<<"Vendedor"<<"\t"<<"Articulo"<<endl;
+
+    string nombreArticulo;
+
+    for (int j = 0; j <= contadorFactura; j++){
+        for (int k = 0;k < sizeof(arregloI)/sizeof(arregloI[0]);k++){
+            if (arregloF[j].Cve_Articulo == arregloI[k].Cve_Articulo)
+                nombreArticulo = arregloI[k].Descripcion;
+        }
+        cout <<arregloF[j].NoFactura<<"\t"
+            <<arregloF[j].Cve_Vendedor<<"\t\t"
+            <<arregloF[j].Cve_Articulo<<"\t"
+            <<nombreArticulo<<endl;
+    }
+
+    return 0;
+}
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# TC1030
+# TC1030 Mayo-Junio 2021
+
+Este repositorio será utilizado para los trabajos de la materia TC1030 Programación Orientada a Objetos.
+
+## Integrantes
+
+- María Carelia Ibarra Vásquez A01251463 ITC
+- Fernando González Rosas A01253694 IDM
+
+## Contenidos
+
+- Actividad 1: Factura en Github
