e8b27591a51a4d1e6d3bb67c85dbe107f7707554
Carelia Ibarra <carelia2001@hotmail.com>
2021-05-13 13:34:55 -0700

    [UPDATE] Act 1
    Updated Vendedor methods and functions.

--- Act1_A01251463.cpp
+++ Act1_A01251463.cpp
@@ -1,13 +1,55 @@
+// Maria Carelia Ibarra y Fernando Gonzalez
+// A01251463 A01253694
+// 13 de mayo del 2021
+// TC1030.700
+
 #include <iostream>
 #include <ctime>
 #include <windows.h>
-
+#include <string>
+#include <sstream> // for stringstream for tokenizing
+#include <fstream> // open an archive
+#include <vector>
 using namespace std;
 
 class Vendedor {                    // clase vendedor
-    public:
+        private:
         string Cve_Vendedor;        // clave del vendedor
         string Nombre;              // nombre del vendedor
+        
+        public:
+        void setDatos(string clave, string nombre){
+            Cve_Vendedor = clave;
+            Nombre = nombre;
+
+        }
+        string getClave(){return Cve_Vendedor;}
+        string getNombre(){return Nombre;}
+};
+
+void agregarVendedor(vector <Vendedor> &vecV){
+    int no, i;
+    string claveVen, nombreVen;
+    Vendedor ven;
+
+    cout << "Ingrese el numero de vendedores a ingresar por favor." << endl;
+    cin >> no;
+
+    while (no <= 0){
+        cout << "Ingrese un numero mayor a cero." << endl;
+        cin >> no;
+    };
+
+    for (i = 0; i < no; i++){
+        cout << "Clave del vendedor:" << endl;
+        cin >> claveVen;
+        cout << "Nombre del vendedor:" << endl;
+        cin >> nombreVen;
+
+        ven.setDatos(claveVen, nombreVen);
+
+        vecV.push_back(ven);
+    }
 };
 
 class Inventario {
@@ -33,10 +75,10 @@ string GETDATE(){
     return out;
 };
 
-void GeneraFactura(int contadorFactura, Factura arregloF[], Vendedor v1, Inventario i2, int Cant){
+void GeneraFactura(int contadorFactura, Factura arregloF[], vector <Vendedor> &vecV , Inventario i2, int Cant){
 
     Factura f1;
-    f1.Cve_Vendedor = v1.Cve_Vendedor;
+    f1.Cve_Vendedor = vecV[1].getClave();
     f1.Cve_Articulo = i2.Cve_Articulo;
     f1.Cantidad = Cant;
     f1.NoFactura = "F" + GETDATE();
@@ -45,14 +87,16 @@ void GeneraFactura(int contadorFactura, Factura arregloF[], Vendedor v1, Inventa
 
 int main() {
 
-    Vendedor arregloV[2];
+    vector <Vendedor> vecVendedor;
     Inventario arregloI[4];
     Factura arregloF[5];
 
-    Vendedor v1 = {"v100","Don Julio Estrella el Grande"}; // variable v1, vendedor 1
+    /*Vendedor v1 = {"v100","Don Julio Estrella el Grande"}; // variable v1, vendedor 1
     Vendedor v2 = {"v200","Dona Esperanza Luna"};
     arregloV[0] = v1;           // metiendo los vendedores al arreglo
-    arregloV[1] = v2;
+    arregloV[1] = v2;*/
+
+    agregarVendedor(vecVendedor);
 
     Inventario i1 = {"i500","Tornillo sin fin",10.0};
     Inventario i2 = {"i100","Pinon Cremallera",10.0};
@@ -63,7 +107,7 @@ int main() {
 
     int contadorFactura = 0;
     
-    GeneraFactura(contadorFactura, arregloF, v1, i2, 32);
+    GeneraFactura(contadorFactura, arregloF, vecVendedor , i2, 32);
 
     // imprimimos todas las facturas
     cout << "Facturas Generadas "<< endl;
